cd5d68b42042150ab0554b15714672b49eb396f7
Daniel Sullivan <ojojoj89@gmail.com>
2023-10-21 21:14:34 -0500

    added missing middleware, and added comments to code

--- Develop/server.js
+++ Develop/server.js
@@ -1,56 +1,61 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const PORT = 3001;
 
 const app = express();
-
+const PORT = 3001;
+//Middleware for parsing JSON and urlencoded form data
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get()("*", (req, res) =>
+//Get route for the main page
+app.get("/", (req, res) => {
 res.sendFile(path.join(__dirname, './public/index.html'))
-);
-
-app.get()("/notes", (req, res) =>
+});
+//Get route for the notes page
+app.get("/notes", (req, res) => {
 res.sendFile(path.join(__dirname, './public/notes.html'))
-);
-
-app.get()("/api/notes", (req, res) =>
+});
+//Get route for the notes api
+app.get("/api/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
         if (err) throw err;
-        const notes = JSON.parse(data);
-        res.json(notes);
+        const savedNotes = JSON.parse(data);
+        res.json(savedNotes);
     })
-);
-app.post()("/api/notes", (req, res) => {
+});
+//Post route for the notes api. This will add a new note to the db.json file
+app.post("/api/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
         if (err) throw err;
-        const notes = JSON.parse(data);
+        const savedNotes = JSON.parse(data);
+        //create a new note with the data from the request body
         const newNote = {
             title: req.body.title,
             text: req.body.text,
-            id: notes.length + 1,
+            id: savedNotes.length + 1,
         }
-        notes.push(newNote);
-        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+        //add the new note to the array of note objects
+        savedNotes.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(savedNotes), (err) => {
             if (err) throw err;
-            res.json(notes);
+            res.json(savedNotes);
         });
     });
 });
-app.delete()("/api/notes/:id", (req, res) => {
+//Delete route for the notes api. This will delete a note from the db.json file
+app.delete("/api/notes/:id", (req, res) => {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
         if (err) throw err;
-        const notes = JSON.parse(data);
-        const newNotes = notes.filter((note) => note.id != req.params.id);
+        const savedNotes = JSON.parse(data);
+        const newNotes = savedNotes.filter((note) => note.id != req.params.id);
         fs.writeFile("./db/db.json", JSON.stringify(newNotes), (err) => {
             if (err) throw err;
             res.json(newNotes);
         });
     });
 });
-
-app.listen(PORT, () =>
+//listen on PORT (in our case, 3001)
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-);
+});
